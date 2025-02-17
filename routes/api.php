b373450b5248f05f109b8297db1d925c6f5297b6
DanielDeSousaDEV <danieldesousa.dev@gmail.com>
2025-02-17 19:04:04 -0300

    atualizando varias coisas

--- routes/api.php
+++ routes/api.php
@@ -6,9 +6,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ConsultaController;
 use App\Http\Controllers\PacienteController;
 
-use App\Models\Paciente;
-
-
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
